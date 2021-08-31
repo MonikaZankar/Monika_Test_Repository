6a415cbeaac8536dd2de8e76f2c3b055c4674d67
Monika Zankar <monikazankar@gmail.com>
2021-08-31 11:20:32 -0700

    Resolved Conflict

--- src/Pkg1/Test1.java
+++ src/Pkg1/Test1.java
@@ -5,5 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World.....123");
 		System.out.println("Welcom to Eclipse");
+		System.out.println("Welcome to Java");
+		System.out.println("Practicing Conflict Scenarioes");
+		
 	}
 }
