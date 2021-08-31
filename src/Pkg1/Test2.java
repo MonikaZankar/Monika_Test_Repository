66b29d3a34ba5b8f439066a2d43f92d824912d0d
Monika Zankar <monikazankar@gmail.com>
2021-08-31 11:32:59 -0700

    Updating

--- src/Pkg1/Test2.java
+++ src/Pkg1/Test2.java
@@ -1,7 +1,11 @@
 package Pkg1;
 
 public class Test2 {
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 	System.out.println("Welcome to Eclipse");
+	System.out.println("@@@@@@@@@@@");
+	System.out.println("hello");
+	System.out.println("##########");
 }
 }
